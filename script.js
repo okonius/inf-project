1652f541e0ecc692bb087a994d1cd6f25d874fac
okonius <87233476+okonius@users.noreply.github.com>
2026-04-17 08:59:43 +0000

    stworzenie strony

---
+++ script.js
@@ -0,0 +1,24 @@
+const text = "Ślady Gminy Trawniki";
+const speed = 75;
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    const titleElement = document.getElementById("title");
+    
+    // Tworzymy span dla litery
+    const span = document.createElement("span");
+    span.className = "letter";
+    
+    // Obsługa spacji - jeśli znak to spacja, używamy twardej spacji, 
+    // żeby HTML nie "zjadł" odstępu między spanami
+    span.innerHTML = text.charAt(i) === " " ? "&nbsp;" : text.charAt(i);
+    
+    titleElement.appendChild(span);
+    
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter;
