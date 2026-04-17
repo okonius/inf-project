f6e8de7f9c9294e52711b4ce0039fa70d04531c2
okonius <okonskykuba09@gmail.com>
2026-04-17 20:51:22 +0200

    zmainy nowe 1
    sdzd

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const text = "Ślady Gminy Trawniki";
+const text = "Śladami Trawnik";
 const speed = 75;
 let i = 0;
 
 function typeWriter() {
   if (i < text.length) {
     const titleElement = document.getElementById("title");
-    
+
     // Tworzymy span dla litery
     const span = document.createElement("span");
     span.className = "letter";
-    
-    // Obsługa spacji - jeśli znak to spacja, używamy twardej spacji, 
+
+    // Obsługa spacji - jeśli znak to spacja, używamy twardej spacji,
     // żeby HTML nie "zjadł" odstępu między spanami
     span.innerHTML = text.charAt(i) === " " ? "&nbsp;" : text.charAt(i);
-    
+
     titleElement.appendChild(span);
-    
+
     i++;
     setTimeout(typeWriter, speed);
   }
